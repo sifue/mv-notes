f8ea212f004f849081e02c8ab8a86b74ed4d1b44
Soichiro Yoshimura <yoshimura@soichiro.org>
2015-12-05 19:48:12 +0900

    URI設計するところまで

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var express = require('express');
-var app = express();
+(function(){
+  'use strict';
 
-app.set('port', (process.env.PORT || 5000));
+  var express = require('express');
+  var app = express();
+  
+  app.set('port', (process.env.PORT || 5000));
+  
+  app.use(express.static(__dirname + '/public'));
+  
+  
+  app.get('/', function(req, res) {
+    res.send('mv-notes working.');
+  });
+  
+  app.get('/notes/:note_name/posts', function(req, res) {
+    console.log(req.params);
+    let noteName = req.params.note_name;
+    res.send('mv-notes working. noteName:' + noteName);
+    res.json();
+  });
+  
+  app.post('/notes/:note_name/posts', function(req, res) {
+    console.log(req.params);
+    let noteName = req.params.note_name;
+    res.send('mv-notes working. noteName:' + noteName);
+    res.json();
+  });
+  
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
+})();
 
