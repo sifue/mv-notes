d97ca4dee4110a7427b063ea59db4fb202782f8b
Soichiro Yoshimura <yoshimura@soichiro.org>
2015-12-07 09:50:43 +0900

    delete not dll

--- ddl.sql
+++ ddl.sql
@@ -8,13 +8,3 @@ create index posts_note_name_created_at on posts (
   note_name,
   created_at
 );
-
-# drop
-drop table posts cascade;
-
-# follws test query
-insert into posts(note_name, created_at, data) values
- ('test', current_date, '{"name": "test","content": "hello!"}');
-select post_id, note_name, created_at, data from posts
-where note_name = 'test' order by created_at desc limit 5;
-delete from posts where post_id in (select post_id from posts order by post_id desc offset 9999 limit 1);
